6bab9b867caebf5bdc4d378baad4215f5ed98fe9
Alv4r0Dev13 <alvarorenancf00@gmail.com>
2023-01-31 23:21:27 -0300

    Últimas atualizações

--- src/AddRoupa.java
+++ src/AddRoupa.java
@@ -7,24 +7,28 @@ public class AddRoupa {
         Scanner scanner = new Scanner(System.in);
         int resp1;
         do {
-            System.out.println("\nQue tipo de roupa deseja adicionar" +
+            System.out.println("\nQue tipo de roupa deseja adicionar?" +
                     "\n0- Cancelar" +
                     "\n1- Camisa" +
                     "\n2- Vestido" +
-                    "\n3- Chinelo");
+                    "\n3- Chinelo" +
+                    "\n4- Bermuda" +
+                    "\n5- Jaqueta");
             resp1 = scanner.nextInt();
             switch (resp1) {
                 case 0: break;
                 case 1: estoque.addItem(addCamisa()); break;
                 case 2: estoque.addItem(addVestido()); break;
                 case 3: estoque.addItem(addChinelo()); break;
-                case 4:
-                default: System.out.println("Opção inválida!");
+                case 4: estoque.addItem(addBermudas()); break;
+                case 5: estoque.addItem(addJaqueta()); break;
+                default: System.out.println("Opcao invalida!");
             }
-            System.out.println("Deseja adicionar mais uma peça (s/n)?");
+            System.out.println("Deseja adicionar mais uma peca (s/n)?");
             String resp2 = scanner.next();
             resp2 = scanner.nextLine();
             if (resp2.equalsIgnoreCase("s\n")) resp1 = -1;
+            else resp1 = 0;
         } while (resp1 != 0);
     }
 
@@ -39,14 +43,14 @@ public class AddRoupa {
         Scanner scanner = new Scanner(System.in);
         int resp1;
         do {
-            System.out.println("\nQual é o tamanho da manga?\n1- Longa\n2- Curta\n3- Sem manga");
+            System.out.println("\nQual eh o tamanho da manga?\n1- Longa\n2- Curta\n3- Sem manga");
             resp1 = scanner.nextInt();
 
             switch (resp1) {
                 case 1: manga = "longa"; break;
                 case 2: manga = "curta"; break;
                 case 3: manga = "sem manga"; break;
-                default: System.out.println("Opção inválida!");
+                default: System.out.println("Opcao invalida!");
             }
         } while (resp1 < 1 || resp1 > 3);
 
@@ -61,7 +65,6 @@ public class AddRoupa {
 
         return new Camisa(nome, tamanhos, estampa, corDaRoupa, precoMin, fator, manga, gola);
     }
-    
 
     public Roupa addVestido() {
         String nome = setNome();
@@ -71,7 +74,22 @@ public class AddRoupa {
         String tamanhoSaia = null;
         String manga = null;
 
-        // Resto do método
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\nInforme o tamanho da saia: ");
+        tamanhoSaia = scanner.next();
+
+        int resp1;
+        do {
+            System.out.println("\nQual eh o tamanho da manga?\n1- Longa\n2- Curta\n3- Sem manga");
+            resp1 = scanner.nextInt();
+
+            switch (resp1) {
+                case 1: manga = "longa"; break;
+                case 2: manga = "curta"; break;
+                case 3: manga = "sem manga"; break;
+                default: System.out.println("Opcao invalida!");
+            }
+        } while (resp1 < 1 || resp1 > 3);
 
         double precoMin = setPrecoMin();
         double fator = setFator();
@@ -89,18 +107,18 @@ public class AddRoupa {
         int tamMin = 0;
         int tamMax = 0;
 
-        System.out.println("Qual eh o material?");
+        System.out.println("\nQual eh o material?");
         material = scanner.next();
 
-        System.out.println("Informe o valor do tamanho minimo:");
+        System.out.println("\nInforme o valor do tamanho minimo:");
         tamMin = scanner.nextInt();
 
-        System.out.println("Informe o valor do tamanho maximo:");
+        System.out.println("\nInforme o valor do tamanho maximo:");
         tamMax = scanner.nextInt();
 
         double precoMin = setPrecoMin();
         double fator = setFator();
-        
+
         return new Chinelo(nome, estampa, corDaRoupa, precoMin, fator, material, tamMin, tamMax);
     }
 
@@ -119,17 +137,17 @@ public class AddRoupa {
         resp1 = scanner.nextLine();
         if (resp1.equalsIgnoreCase("s")) bolso = true;
         else bolso = false;
-        
+
         int resp2;
         do {
-            System.out.println("\nQual é o gênero da peça?\n1- Masculina\n2- Feminina\n3- Unissex");
+            System.out.println("\nQual eh o genero da peca?\n1- Masculina\n2- Feminina\n3- Unissex");
             resp2 = scanner.nextInt();
 
             switch (resp2) {
                 case 1: genero = "masculina"; break;
                 case 2: genero = "feminina"; break;
                 case 3: genero = "unissex"; break;
-                default: System.out.println("Opção inválida!");
+                default: System.out.println("Opcao invalida!");
             }
         } while (resp2 < 1 || resp2 > 3);
 
@@ -137,6 +155,33 @@ public class AddRoupa {
         double fator = setFator();
 
         return new Bermudas(nome, tamanhos, estampa, corDaRoupa, precoMin, fator, bolso, genero);
+    }
+
+    public Roupa addJaqueta() {
+        String nome = setNome();
+        List<String> tamanhos = setTamanhos();
+        CorDaRoupa corDaRoupa = setCor();
+        Boolean estampa = hasEstampa();
+        String manga = "";
+
+        Scanner scanner = new Scanner(System.in);
+        int resp;
+        do {
+            System.out.println("\nQual é o tamanho da manga?\n1- Longa\n2- Curta\n3- Sem manga");
+            resp = scanner.nextInt();
+
+            switch (resp) {
+                case 1: manga = "longa"; break;
+                case 2: manga = "curta"; break;
+                case 3: manga = "sem manga"; break;
+                default: System.out.println("Opção inválida!");
+            }
+        } while (resp < 1 || resp > 3);
+        
+        double precoMin = setPrecoMin();
+        double fator = setFator();
+
+        return new Jaqueta(nome, tamanhos, estampa, corDaRoupa, manga, precoMin, fator);
     }
 
     /** MÉTODOS GERAIS */
@@ -150,7 +195,7 @@ public class AddRoupa {
     private List<String> setTamanhos() {
         List<String> tamanhos = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Insira os tamanhos disponíveis (para encerrar digite '0'):");
+        System.out.println("Insira os tamanhos disponíveis, do menor para o maior (para encerrar digite '0'):");
         String resp;
 
         do {
@@ -165,7 +210,7 @@ public class AddRoupa {
         CorDaRoupa corDaRoupa = null;
         int resp;
         do {
-            System.out.println("Qual a cor da peça?");
+            System.out.println("Qual a cor da peca?");
             for (CorDaRoupa cor : CorDaRoupa.values()) {
                 System.out.printf("%d- %s%n", cor.ordinal() + 1, cor.name().toLowerCase());
             }
@@ -181,7 +226,7 @@ public class AddRoupa {
                 case 6: corDaRoupa = CorDaRoupa.AZUL; break;
                 case 7: corDaRoupa = CorDaRoupa.ROSA; break;
                 case 8: corDaRoupa = CorDaRoupa.AMARELO; break;
-                default: System.out.println("Opção inválida!");
+                default: System.out.println("Opcao invalida!");
             }
         } while (resp < 1 || resp > CorDaRoupa.values().length);
 
